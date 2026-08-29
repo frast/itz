d33a9f8462f0063baac3cc88b3eaddfdb54ce85e
Frank Rast <frank.rast@gmail.com>
2026-08-29 22:51:02 +0000

    use injection

--- adapters/primary/rest/src/main/java/de/itz/adapter/primary/rest/PingResource.java
+++ adapters/primary/rest/src/main/java/de/itz/adapter/primary/rest/PingResource.java
@@ -2,6 +2,7 @@ package de.itz.adapter.primary.rest;
 
 import de.itz.application.PingUseCase;
 import de.itz.domain.Ping;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -10,6 +11,15 @@ import jakarta.ws.rs.core.MediaType;
 @Path("ping")
 @Produces(MediaType.APPLICATION_JSON)
 public class PingResource {
-    private final PingUseCase useCase = new PingUseCase();
+    private final PingUseCase useCase;
+
+    public PingResource() {
+        this.useCase = null;
+    }
+
+    @Inject
+    public PingResource(PingUseCase useCase) {
+        this.useCase = useCase;
+    }
     @GET public Ping ping() { return useCase.execute(); }
 }
--- application/usecases/src/main/java/de/itz/application/PingUseCase.java
+++ application/usecases/src/main/java/de/itz/application/PingUseCase.java
@@ -1,7 +1,9 @@
 package de.itz.application;
 
 import de.itz.domain.Ping;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class PingUseCase {
     public Ping execute() {
         return Ping.alive();
